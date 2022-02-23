291f95bf895a0f0beb02a254a1cc25ad0058f935
Matt Wayland <mw283@cam.ac.uk>
2022-02-23 11:46:48 +0000

    Update LiquidCrystal_I2C_MTW.h

--- LiquidCrystal_I2C_MTW.h
+++ LiquidCrystal_I2C_MTW.h
@@ -57,7 +57,7 @@
  * The backlight is on by default, since that is the most likely operating mode in
  * most cases.
  */
-class LiquidCrystal_I2C : public Print {
+class LiquidCrystal_I2C_MTW : public Print {
 public:
 	/**
 	 * Constructor
@@ -68,7 +68,7 @@ public:
 	 * @param lcd_rows	Number of rows your LCD display has.
 	 * @param charsize	The size in dots that the display has, use LCD_5x10DOTS or LCD_5x8DOTS.
 	 */
-	LiquidCrystal_I2C(uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t charsize = LCD_5x8DOTS);
+	LiquidCrystal_I2C_MTW(uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t charsize = LCD_5x8DOTS);
 
 	/**
 	 * Set the LCD display in the correct begin state, must be called before anything else is done.
